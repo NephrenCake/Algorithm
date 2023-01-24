28670c42c24d2e59e136e8904d9d000c05cba1df
NephrenCake <834957063@qq.com>
2023-01-25 02:00:01 +0800

    你交机试day8 再不学要寄了

--- AcWing/算法提高课/1.3.3 背包模型（三）/487. 金明的预算方案.cpp
+++ AcWing/算法提高课/1.3.3 背包模型（三）/487. 金明的预算方案.cpp
@@ -8,39 +8,76 @@ using namespace std;
 typedef pair<int, int> PII;
 
 const int N = 70, M = 32010;
-int m, n, f[N][M];
+int n, m, f[M];
 PII fa[N];
 vector<PII> son[N];
 
+// 2. 一维优化
 int main() {
     cin >> m >> n;
     for (int i = 1; i <= n; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        if (!c) fa[i] = {a, a * b};
-        else son[c].push_back({a, a * b});
+        if (!c) fa[i] = {a, b * a};
+        else son[c].push_back({a, b * a});
     }
 
-    for (int i = 1; i <= n; i++) {  // 前i个物品
-        if (fa[i].v) {  // 如果第i个物品是主件
-            // 01背包，先从主件开始放
-            for (int j = fa[i].v; j <= m; j++)
-                f[i][j] = max(f[i][j], f[i - 1][j - fa[i].v] + fa[i].w);
-
-            // 因为push_back的下标是从0开始的，遍历的时候要注意
-            // 这部分判断的是，放入当前的主件，对于这个主件的附件用01背包的方式去思考求解
-            // 并且当前的f[i][j]一定是已经放入当前主件的情况
-            for (int k = 0; k < son[i].size(); k++)
-                for (int j = m; j >= son[i][k].v; j--)
-                    // 放过主件以后，才能放附件，f为0的部分说明这部分放不进当前的主件
-                    if (f[i][j - son[i][k].v] > 0)
-                        f[i][j] = max(f[i][j], f[i][j - son[i][k].v] + son[i][k].w);
-        }
-        // 因为主件和附件要共同判断，但是在综合考虑之前，无法判断和前一个主件哪个更大
-        // 注意i不是主件时，也要把状态传递下去
-        for (int j = 0; j <= m; j++)
-            f[i][j] = max(f[i][j], f[i - 1][j]);
-    }
-
-    cout << f[n][m] << endl;
+    for (int i = 1; i <= n; i++)  // 前i个物品组。每个物品都有可能是一个组，最多n个物品组，实际是多少个主件
+        for (int j = m; j >= 0; j--)  // 前j的空间
+            if (fa[i].v)  // 可有可无，因为一维dp状态自动转移到下一层
+                for (int k = 0; k < 1 << son[i].size(); k++) {  // 二进制枚举，每位1表示选这个物品0表示不选，k表示第几种策略
+                    // 计算该策略占用v空间，得到w价值
+                    int v = fa[i].v, w = fa[i].w;
+                    for (int l = 0; l < son[i].size(); l++)
+                        if (k >> l & 1) {
+                            v += son[i][l].v;
+                            w += son[i][l].w;
+                        }
+                    if (j - v >= 0)  // 转移方程
+                        f[j] = max(f[j], f[j - v] + w);
+                }
+    cout << f[m] << endl;
 }
+
+// #include <iostream>
+// #include <vector>
+//
+// using namespace std;
+//
+// #define v first
+// #define w second
+// typedef pair<int, int> PII;
+//
+// const int N = 70, M = 32010;
+// int n, m, f[N][M];
+// PII fa[N];  // 物品i为主件时，存在fa[i]
+// vector<PII> son[N];  // 物品j为物品i的附件时，存在son[i]
+//
+// // 1. 朴素二维 二进制枚举
+// int main() {
+//     cin >> m >> n;
+//     for (int i = 1; i <= n; i++) {
+//         int a, b, c;
+//         cin >> a >> b >> c;  // 价格 重要度 主件还是附件
+//         if (!c) fa[i] = {a, a * b};
+//         else son[c].push_back({a, a * b});
+//     }
+//
+//     for (int i = 1; i <= n; i++)
+//         for (int j = 1; j <= m; j++) {
+//             // 当主件i不是主件时，当做一个0价格0价值的物品来保持状态f的转移
+//             f[i][j] = f[i - 1][j];
+//             if (fa[i].v)  // 可有可无
+//                 for (int k = 0; k < 1 << son[i].size(); k++) {
+//                     int v = fa[i].v, w = fa[i].w;
+//                     for (int l = 0; l < son[i].size(); l++)
+//                         if (k >> l & 1) {
+//                             v += son[i][l].v;
+//                             w += son[i][l].w;
+//                         }
+//                     if (j - v >= 0)
+//                         f[i][j] = max(f[i][j], f[i - 1][j - v] + w);
+//                 }
+//         }
+//     cout << f[n][m];
+// }
